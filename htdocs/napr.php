1c094374ba3420dec9c8dc770d6f7775fc1bb74e
NXHhunter2 <usaloveman@mail.ru>
2021-04-09 18:01:58 +0400

    htdocs

---
+++ htdocs/napr.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Академия Калашникова</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="fontawesome/css/all.css">
+</head>
+<body>
+    <header>
+        <div class="first_part">
+            <img src="img/logo_academy.svg" class="logo fp_p">
+            <div class="page_list fp_p">Главная</div>
+        </div>
+        <div class="second_part">
+            <i class="far fa-comments sp_p"></i>
+            <i class="far fa-bell sp_p"></i>
+            <img src="img/avatar.jpg" class="sp_p avatar">
+        </div>
+    </header>
+    <div class="main-box">
+        <menu>
+            <div class="high_name">
+                <div class="high"><i class="fas fa-th-large menu_p"></i onclick='location.href="index.php"'>Главная</div>
+                <a>Направление</a>
+                <a>Задачи</a>
+                <a>Расписание</a>
+                <a>Новости</a>
+            </div>
+            <div class="high_name">
+                <div class="high"><i class="fas fa-user-friends menu_p"></i>Одногруппники</div>
+                <a>Чаты</a>
+                <a>Моя группа</a>
+            </div>
+            <div class="high_name">
+                <div class="high"><i class="fas fa-comment menu_p"></i>Поддержка</div>
+                <a>FAQ</a>
+                <a>Задать вопрос</a>
+            </div>
+        </menu>
+        <div class="cont">    
+			<section class="course">
+				Курс
+			</section>
+        </div>
+    </div>
+    <footer>
+
+    </footer>
+</body>
+</html>
